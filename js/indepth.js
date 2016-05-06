2a8a25dc8b21b7207b806045630249f6535d3fef
Vocho <vocho@underdog.gs>
2016-05-06 16:51:59 -0500

    eww
    ERA

--- js/indepth.js
+++ js/indepth.js
@@ -881,8 +881,8 @@ $("#indepth_twittear").click(function(){
 
 		
 		var text = encodeURIComponent("Mi predicción es: Pumas "+$("input[name=goleador]").val()+"-"+$("input[name=goleador2]").val())+ " América primer gol al minuto "+$("input[name=goles_anotados]").val()+" @juanfutbol";
-		var url = encodeURIComponent("http://juanfutbol.com/indepth/");
-		window.open("https://twitter.com/share?text="+text+"&hashtags=pizzafut&url="+url,"indepth/juanpizza-pumas-america","width=500, height=300");
+		var url = encodeURIComponent("http://juanfutbol.com/indepth/indepth/juanpizza-pumas-america");
+		window.open("https://twitter.com/share?text="+text+"&hashtags=pizzafut&url="+url,"width=500, height=300");
 
 	}else{
 		
