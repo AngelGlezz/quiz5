88334ce21e6800600b5dc8c4c0a1c07f7507fc81
AngelGlezz <angelglezz.7@gmail.com>
2016-05-06 11:58:49 -0500

    2
    asdfgh

--- js/indepth.js
+++ js/indepth.js
@@ -13,7 +13,7 @@ var final_time = 0;
 var aciertos = 0;
 
 
-var maxTime = 930;
+var maxTime = 30;
 var time = maxTime;
 var time_out=0;
 
@@ -882,7 +882,7 @@ $("#indepth_twittear").click(function(){
 		
 		var text = encodeURIComponent("Mi predicción es: Pumas "+$("input[name=goleador]").val()+"-"+$("input[name=goleador2]").val())+ " América primer gol al minuto "+$("input[name=goles_anotados]").val()+" @juanfutbol";
 		var url = encodeURIComponent("http://juanfutbol.com/indepth/");
-		window.open("https://twitter.com/share?text="+text+"&hashtags=juanfutbol&url="+url,"","width=500, height=300");
+		window.open("https://twitter.com/share?text="+text+"&hashtags=pizzafut&url="+url,"indepth/juanpizza-pumas-america","width=500, height=300");
 
 	}else{
 		
